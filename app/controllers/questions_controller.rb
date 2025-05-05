c2ec485ac3ab09a8d890ed50f4d4cdf23dd0fce7
manel boulaabi <nuwr24@gmail.com>
2025-05-05 14:40:21 +0100

    question/answer created

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    # raise
+    @question = params[:question]
+
+      if @question == "I am going to work"
+        return @answer = "Great!"
+      elsif @question.end_with?("?")
+        return @answer = "Silly question, get dressed and go to work!."
+      else
+        return @answer = "I don't care, get dressed and go to work!"
+      end
+  end
+end
