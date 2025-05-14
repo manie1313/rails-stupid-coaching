5fe2f3f3d49abe4e0680cfb930cd7bb325f9b91d
manel boulaabi <nuwr24@gmail.com>
2025-05-14 16:02:00 +0100

    Update: clearing the history key, to do where questions and answers keys come from

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,38 +1,45 @@
 class QuestionsController < ApplicationController
   def ask
+    # session.delete(:history)
   end
+
+
+# reset_session ==> clear session temporarely
 
   def answer
-    # raise
 
-    if question = params[:question] !=nil
+    question = params[:question]
+    # session.delete(:history)
+    # session[:history] = []
+    if params[:question].present?
       session[:history] << {
-        question: "question",
-        answer: coach_answer(question)
+        "question" => question,
+        "answer" => coach_answer(question)
       }
+      # raise
     end
+
     # session[:history][0]["question"] ==> 1st question
-    # session[:history][i]["answer"] ==> 1st answer
+    # session[:history][0]["answer"] ==> 1st answer
+    @hey = "YYYYYYYY"
+    raise
     @questions = session[:history].map { |convo| convo["question"] }
     @answers = session[:history].map { |convo| convo["answer"] }
-
-
   end
 
-  private
-
   def coach_answer(question)
-    case question
-    when 'I am going to work' then 'Great!'
+    case question.downcase
+    when 'i am going to work' then 'Great!'
     when /\?$/ then "Silly question, get dressed and go to work!."
     else "I don't care, get dressed and go to work!"
     end
-    # if question == "I am going to work"
-    #   return answer = "Great!"
-    # elsif question.end_with?("?")
-    #   return answer = "Silly question, get dressed and go to work!."
-    # else
-    #   return answer = "I don't care, get dressed and go to work!"
-    # end
   end
+  #   if question == "I am going to work"
+  #     return answer = "Great!"
+  #   elsif question.end_with?("?")
+  #     return answer = "Silly question, get dressed and go to work!."
+  #   else
+  #     return answer = "I don't care, get dressed and go to work!"
+  #   end
+  # end
 end
