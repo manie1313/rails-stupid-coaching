017e3c3069456d8857acefaedf44950502bf92e2
manel boulaabi <nuwr24@gmail.com>
2025-05-14 14:47:29 +0100

    update: making it look better

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,14 +4,35 @@ class QuestionsController < ApplicationController
 
   def answer
     # raise
-    @question = params[:question]
 
-      if @question == "I am going to work"
-        return @answer = "Great!"
-      elsif @question.end_with?("?")
-        return @answer = "Silly question, get dressed and go to work!."
-      else
-        return @answer = "I don't care, get dressed and go to work!"
-      end
+    if question = params[:question] !=nil
+      session[:history] << {
+        question: "question",
+        answer: coach_answer(question)
+      }
+    end
+    # session[:history][0]["question"] ==> 1st question
+    # session[:history][i]["answer"] ==> 1st answer
+    @questions = session[:history].map { |convo| convo["question"] }
+    @answers = session[:history].map { |convo| convo["answer"] }
+
+
+  end
+
+  private
+
+  def coach_answer(question)
+    case question
+    when 'I am going to work' then 'Great!'
+    when /\?$/ then "Silly question, get dressed and go to work!."
+    else "I don't care, get dressed and go to work!"
+    end
+    # if question == "I am going to work"
+    #   return answer = "Great!"
+    # elsif question.end_with?("?")
+    #   return answer = "Silly question, get dressed and go to work!."
+    # else
+    #   return answer = "I don't care, get dressed and go to work!"
+    # end
   end
 end
